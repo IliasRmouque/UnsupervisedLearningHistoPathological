67750c532303d9c759551831ae65ce95fb086680
IliasRmouque <ilias.rmouque@gmail.com>
2020-11-20 18:06:07 +0100

    AnnotationMasks completed

--- AnnotationMasks.py
+++ AnnotationMasks.py
@@ -5,69 +5,87 @@ import sys
 import warnings
 from argparse import ArgumentParser
 import os
-import csv
-import shapely.wkt
-from PIL import Image
-from PIL.ImageDraw import ImageDraw
+from PIL import Image, ImageDraw
+from cytomine.models import  AnnotationCollection,  ImageInstanceCollection
 from cytomine import Cytomine
-from cytomine.models import AnnotationCollection
-from patches import Patch
-from shapely import wkt
-from shapely.geometry import Polygon
-from cytomine.models import (AnnotationCollection, ImageInstance,
-                             ImageInstanceCollection)
-import requests
-from cytomine.models import Property, Annotation, AnnotationTerm, AnnotationCollection
-from cytomine import Cytomine
-import shutil
+
 host = "http://cytomine.icube.unistra.fr"
 public_key = "8da00e26-3bcb-4229-b31d-a2b5937c4e5e"  # check your own keys from your account page in the web interface
 private_key = "c0018f6a-8aa1-4791-957b-ab72dce4238d"
-Dl_Path = "../Repertoir_Travail/annoPoly2"
-PROJECT_ID = '1345'
-IM_SCALE_FACTOR = 1
-DCT= {}
-DCT1={}
 
 
-def get_object_mask_per_image(image_id, project_id, annotation_id,
-                              scale_factor, output_filename):
-    res = 1
-    image_instances = ImageInstanceCollection().fetch_with_filter("project", project_id)
-    image_instance = 0
-    for im in image_instances:
-        if(int(im.id)== int(image_id)):
-            image_instance = im
+if __name__ == '__main__':
+    parser = ArgumentParser(prog="Cytomine Python client example")
 
-    image_width = int(image_instance.width * scale_factor)
-    image_height = int(image_instance.height * scale_factor)
+    parser.add_argument('--proj_id', dest='id_project',
+                        help="The project from which we want the annotations")
 
-    annotations = AnnotationCollection()
-    annotations.project = project_id
-    annotations.image = image_id
-    annotations.fetch()
-    if len(annotations) <= 0:
-        return -1
-    result_image = Image.new(mode='1', size=(image_width, image_height), color=0)
+    parser.add_argument('--img_id', dest='id_image',
+                        help="The images from which we want the annotations")
 
-    somethingannotated = False
-    for annotation in annotations:  # for each annotation (i.e. polygone)
-        annotation.fetch()
+    parser.add_argument('--dest', dest='dest', required=False,
+                        help="Where to store images")
 
-        if not annotation.term:
-            #raise ValueError("Annotation %d has not been associated with a term" % annotation.id)
-            warnings.warn("Annotation %d has not been associated with a term" % annotation.id)
+    parser.add_argument('--downscale',dest='down', required=False,
+                        help="the downscale factor, 10 if not given")
 
-        # Get the polygon coordinates from cytomine
-        if int(annotation_id) in annotation.term:  #  if the object is the one we want to extract
-            if annotation.location.startswith("POLYGON") or annotation.location.startswith("MULTIPOLYGON"):
-                somethingannotated = True
-                if annotation.location.startswith("POLYGON"):
+    params, other = parser.parse_known_args(sys.argv[1:])
+
+    '--proj_id 1345 --img_id 6522463 --dest ./ploud/ --downscale 100'
+
+
+    with  Cytomine(host=host, public_key=public_key, private_key=private_key,
+              verbose=logging.INFO) as cytomine:
+
+        if params.down:
+            scale_factor = 1 / int(params.down)
+        else :
+            scale_factor = 1 / 10
+        proj_id = int(params.id_project)
+        image_id = int(params.id_image)
+        if params.dest:
+            os.makedirs(params.dest, exist_ok=True)
+
+
+        im = ImageInstanceCollection()
+        im.project =proj_id
+        im.image = image_id
+        im.fetch_with_filter("project", proj_id)
+        image_width = int(im[0].width)
+        image_height = int(im[0].height)
+        print(image_height, image_width)
+
+        annotations = AnnotationCollection()
+        annotations.project = proj_id
+        annotations.image = image_id
+        # if needed
+        # annotations.user = user_id
+        annotations.showWKT = True
+        annotations.showMeta = True
+        annotations.showTerm = True
+        annotations.showGIS = True
+        annotations.showImage = True
+        annotations.showUser = True
+        annotations.fetch( )
+
+        dct_anotations = {}
+        for a in annotations:
+            if len(a.term) == 1:
+                term = a.term[0]
+                if term not in dct_anotations:
+                    dct_anotations[term]=[]
+                dct_anotations[term].append(a.location)
+            else:
+                warnings.warn("Not suited for multiple or no annotation term")
+        for t, lanno in dct_anotations.items():
+            result_image = Image.new(mode='1', size=( int(image_width* scale_factor), int(image_height * scale_factor)), color=0)
+            for pwkt in lanno:
+                if pwkt.startswith("POLYGON"):
                     label = "POLYGON"
-                elif annotation.location.startswith("MULTIPOLYGON"):
+                elif pwkt.startswith("MULTIPOLYGON"):
                     label = "MULTIPOLYGON"
 
-                coordinatesStringList = annotation.location.replace(label, '')
+                coordinatesStringList = pwkt.replace(label, '')
 
                 if label == "POLYGON":
                     coordinates_string_lists = [coordinatesStringList]
@@ -83,39 +101,19 @@ def get_object_mask_per_image(image_id, project_id, annotation_id,
                     y_coords = []
                     for point in coordinatesStringList.split(','):
                         point = point.strip(string.whitespace)  # remove leading and ending spaces
-                        point = point.strip(string.punctuation) # Have seen some strings have a ')' at the end so remove it
+                        point = point.strip(
+                            string.punctuation)  # Have seen some strings have a ')' at the end so remove it
                         x_coords.append(round(float(point.split(' ')[0])))
                         y_coords.append(round(float(point.split(' ')[1])))
 
                     x_coords_correct_lod = [int(x * scale_factor) for x in x_coords]
-                    y_coords_correct_lod = [image_height - int(x * scale_factor) for x in y_coords]
+                    y_coords_correct_lod = [image_height * scale_factor - int(x * scale_factor) for x in y_coords]
                     coords = [(i, j) for i, j in zip(x_coords_correct_lod, y_coords_correct_lod)]
 
                     #  draw the polygone in an image and fill it
                     ImageDraw.Draw(result_image).polygon(coords, outline=1, fill=1)
 
-    if somethingannotated:
-        result_image.save(output_filename)
-        res = 0
-    return res
-
-
-if __name__ == '__main__':
-    with  Cytomine(host=host, public_key=public_key, private_key=private_key,
-              verbose=logging.INFO) as cytomine:
-        annotations = AnnotationCollection()
-        annotations.project = 1345
-        annotations.image = 10847
-        annotations.jobForTermAlgo = 'tumor'
-        annotations.showWKT = True
-        annotations.showMeta = True
-        annotations.showTerm = True
-        annotations.showGIS = True
-        annotations.showImage = True
-        annotations.showUser = True
-        annotations.fetch( )
-        for a in annotations:
-            print(a.image, a.term)
+            result_image.save(params.dest + '/' + str(t)+'.png')
 
 
 
